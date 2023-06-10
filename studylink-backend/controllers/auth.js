f2a57b260511009ac073ee10c885d7ee9c40b46e
Akuna444 <aklilsolomon3@gmail.com>
2023-06-11 00:01:45 +0300

    update: auth controller

--- studylink-backend/controllers/auth.js
+++ studylink-backend/controllers/auth.js
@@ -48,7 +48,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ msg: "User doesn't exist" });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
