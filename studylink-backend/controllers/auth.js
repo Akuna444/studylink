3ea3aab05fb002f6388e5c2fd485e1d1d8fb0ff5
Akuna444 <aklilsolomon3@gmail.com>
2023-06-10 22:42:22 +0300

    fix: minor server errors

--- studylink-backend/controllers/auth.js
+++ studylink-backend/controllers/auth.js
@@ -49,11 +49,11 @@ export const login = async (req, res) => {
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
-    res.statur(200).json({ token, user });
+    res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
