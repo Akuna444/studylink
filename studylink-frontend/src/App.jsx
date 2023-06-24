a16a5f937ae3b4372b9cef09a9735f4faa8d8344
Akuna444 <aklilsolomon3@gmail.com>
2023-06-24 11:18:58 +0300

    fix: route

--- studylink-frontend/src/App.jsx
+++ studylink-frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
             />
             <Route
               path="/profile/:userId"
-              elemnt={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
             />
           </Routes>
         </ThemeProvider>
